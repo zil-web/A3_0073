167af821bf5aabf790417f3f037e8c964f8f9e7a
zil-web <126854462+zil-web@users.noreply.github.com>
2025-01-21 15:57:50 +0700

    menambahkan data class pemasok untuk dipanggil di view model

---
+++ app/src/main/java/com/example/uaspam/model/Pemasok.kt
@@ -0,0 +1,10 @@
+package com.example.uaspam.model
+
+data class Pemasok (
+    val id_pemasok: String,
+    val nama_produk: String,
+    val nama_pemasok: String,
+
+    val alamat_pemasok: String,
+    val telepon_pemasok: String,
+)
