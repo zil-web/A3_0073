f0b48ae3e5da51ef75318990da1efc7076754c8c
zil-web <126854462+zil-web@users.noreply.github.com>
2025-01-21 15:43:13 +0700

    Class ini  digunakan untuk  informasi tentang produk, seperti inventaris produk di toko .

---
+++ app/src/main/java/com/example/uaspam/model/Produk.kt
@@ -0,0 +1,14 @@
+package com.example.uaspam.model
+
+
+@Serializable
+data class Produk (
+    val id_produk: String,
+    val nama_produk: String,
+    val deskripsi_produk: String,
+
+    val stok: String,
+    val id_kategori: String,
+    val id_pemasok: String,
+    val id_merk: String,
+)
