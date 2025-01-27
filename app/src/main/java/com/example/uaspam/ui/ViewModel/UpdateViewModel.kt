94ac2d210857829465eb833e11d0361113f08d4d
zil-web <126854462+zil-web@users.noreply.github.com>
2025-01-27 16:49:02 +0700

    update view untuk menambahkan relasi dan menambahan parameter

--- app/src/main/java/com/example/uaspam/ui/ViewModel/UpdateViewModel.kt
+++ app/src/main/java/com/example/uaspam/ui/ViewModel/UpdateViewModel.kt
@@ -1,12 +1,18 @@
 package com.example.uaspam.ui.ViewModel
 
+import PemasokRepository
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.uaspam.model.Kategori
+import com.example.uaspam.model.Merk
+import com.example.uaspam.model.Pemasok
 import com.example.uaspam.model.Produk
+import com.example.uaspam.repository.KategoriRepository
+import com.example.uaspam.repository.MerkRepository
 import com.example.uaspam.repository.ProdukRepository
 import com.example.uaspam.ui.Navigation.DestinasiUpdate
 import kotlinx.coroutines.flow.StateFlow
@@ -14,10 +20,37 @@ import kotlinx.coroutines.launch
 
 class UpdateViewModel(
     savedStateHandle: SavedStateHandle,
-    private val produkRepository: ProdukRepository
+    private val produkRepository: ProdukRepository,
+    private val pmsk: PemasokRepository,
+    private val ktgr: KategoriRepository,
+    private val mrk: MerkRepository
+
+
 ): ViewModel(){
     var updateUiState by mutableStateOf(InsertUiState())
         private set
+
+    var pmskList by mutableStateOf<List<Pemasok>>(emptyList())
+    var ktgrList by mutableStateOf<List<Kategori>>(emptyList())
+    var mrkList by mutableStateOf<List<Merk>>(emptyList())
+
+
+    init {
+        //memuat data
+        simpandata()
+    }
+
+    private fun simpandata() {
+        viewModelScope.launch {
+            try {
+                pmskList = pmsk.getpemasok().data
+                ktgrList = ktgr.getkategori().data
+                mrkList = mrk.getmerk().data
+            } catch (e: Exception) {
+
+            }
+        }
+    }
 
     private val _id_produk: String = checkNotNull(savedStateHandle[DestinasiUpdate.ID_produk])
 
