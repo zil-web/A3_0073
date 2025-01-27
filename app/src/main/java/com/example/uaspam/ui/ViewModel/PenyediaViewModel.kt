a1ed0fb95436465b37b7b3fd7f95b7e037729bbe
zil-web <126854462+zil-web@users.noreply.github.com>
2025-01-27 16:25:29 +0700

    membuat initializer untuk tabel merk agar masuk ke js

--- app/src/main/java/com/example/uaspam/ui/ViewModel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/uaspam/ui/ViewModel/PenyediaViewModel.kt
@@ -10,13 +10,17 @@ import com.example.uaspam.ui.ViewModelKategori.DetailViewModelKategori
 import com.example.uaspam.ui.ViewModelKategori.HomeViewModelKategori
 import com.example.uaspam.ui.ViewModelKategori.InsertViewModelKategori
 import com.example.uaspam.ui.ViewModelKategori.UpdateViewModelKategori
+import com.example.uaspam.ui.ViewmodelMerk.DetailViewModelMerk
+import com.example.uaspam.ui.ViewmodelMerk.HomeViewModelMerk
+import com.example.uaspam.ui.ViewmodelMerk.InsertViewModelMerk
+import com.example.uaspam.ui.ViewmodelMerk.UpdateViewModelMerk
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer { HomeViewModel(prdkApp().container.produkRepository) }
         initializer { InsertViewModel(prdkApp().container.produkRepository,prdkApp().container.pemasokRepository,prdkApp().container.kategoriRepository,prdkApp().container.merkRepository) }
         initializer { DetailViewModel(createSavedStateHandle(),prdkApp().container.produkRepository)}
-        initializer { UpdateViewModel(createSavedStateHandle(),prdkApp().container.produkRepository)}
+        initializer { UpdateViewModel(createSavedStateHandle(),prdkApp().container.produkRepository, prdkApp().container.pemasokRepository,prdkApp().container.kategoriRepository,prdkApp().container.merkRepository)}
 
         initializer { HomeViewModelPemasok(prdkApp().container.pemasokRepository) }
         initializer { InsertViewModelPemasok(prdkApp().container.pemasokRepository) }
@@ -27,6 +31,11 @@ object PenyediaViewModel {
         initializer { InsertViewModelKategori(prdkApp().container.kategoriRepository) }
         initializer { DetailViewModelKategori(createSavedStateHandle(),prdkApp().container.kategoriRepository) }
         initializer { UpdateViewModelKategori(createSavedStateHandle(),prdkApp().container.kategoriRepository) }
+
+        initializer { HomeViewModelMerk(prdkApp().container.merkRepository) }
+        initializer { InsertViewModelMerk(prdkApp().container.merkRepository) }
+        initializer { DetailViewModelMerk(createSavedStateHandle(),prdkApp().container.merkRepository) }
+        initializer { UpdateViewModelMerk(createSavedStateHandle(),prdkApp().container.merkRepository) }
     }
 }
 
